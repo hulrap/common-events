5379746c09d379fabced720eeb76732a6e5f1bb8
hulrap <hulanraphael@gmail.com>
2026-01-09 13:33:10 +0100

    v2.2-map-fix-ssp

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,4 +1,11 @@
 import { MapContainer } from '@/components/Map/MapContainer';
+import { GetServerSideProps } from 'next';
+
+// Force server-side rendering - map requires client-side Google Maps API
+// This prevents Next.js from trying to statically generate this page at build time
+export const getServerSideProps: GetServerSideProps = async () => {
+  return { props: {} };
+};
 
 export default function MapPage() {
   return <MapContainer />;
